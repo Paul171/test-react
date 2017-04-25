19f17e9322ce710fbbffa3a49a469c5f8567354a
Paul LO <paullo@cherrypicks.com>
2017-04-25 09:03:00 +0800

    updated the logic for handling different state in react-redux

--- react-vscode-starter/src/js/app.js
+++ react-vscode-starter/src/js/app.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { bindActionCreators } from 'redux';
+import { connect, Provider } from 'react-redux';
 import { WidgetTable } from './components/widget-table';
-import { WidgetForm } from './components/widget-form'
+import { WidgetForm } from './components/widget-form';
+import { refreshWidgets } from './actions/refresh-widgets';
+import { editWidget, cancelWidget } from './actions/edit-widget';
+import { saveWidget } from './actions/save-widget';
+import { deleteWidget } from './actions/delete-widget';
+import { appStore } from './app-store';
 // const widgetData = [{
 //     id: 1, name: 'Small Red Widget', description: 'A small, red widget.',
 //     color: 'red', size: 'small', quantity: 2
@@ -9,6 +16,16 @@ import { WidgetForm } from './components/widget-form'
 //     id: 2, name: 'Medium Blue Widget', description: 'A medium, blue widget.',
 //     color: 'blue', size: 'medium', quantity: 15
 // }];
+
+//destructuring: allows specific properties of an input object to plucked off as variables
+const mapStateToProps = ({ widgets, editRowId }) => ({ widgets, editRowId });
+const mapDispatchToProps = dispatch => bindActionCreators({
+    refreshWidgets,
+    onEdit: editWidget,
+    onCancel: cancelWidget,
+    onSave: saveWidget,
+    onDelete: deleteWidget,
+}, dispatch);
 class WidgetTool extends React.Component {
     // static propTypes = {
     //     widgets: React.PropTypes.arrayOf(React.PropTypes.shape({
@@ -29,9 +46,27 @@ class WidgetTool extends React.Component {
     }
     //inject code at critical points in a component's lifetime
     componentDidMount() {
-        fetch('http://localhost:3000/widgets')
-            .then(res => res.json())
-            .then(widgets => this.setState({ widgets }));
+        // fetch('http://localhost:3000/widgets')
+        //     .then(res => res.json())
+        //     .then(widgets => this.setState({ widgets }));
+
+        //the return value from the subscribe function is an unsubscribe function
+        this.appStoreUnsubscribe = appStore.subscribe(() => {
+            this.setState({
+                widgets: appStore.getState().widgets,
+            });
+        });
+        this.props.refreshWidgets();
+        appStore.dispatch(refreshWidgets());
+    }
+    static propTypes = {
+        refreshWidgets: React.PropTypes.func,
+        onSave: React.PropTypes.func,
+    };
+
+    //unsubscribe function which is invoked with the component is unmounted to prevent memory leaks
+    componentWillUnmount() {
+        this.appStoreUnsubscribe();
     }
     editWidget = widgetId => {
         this.setState({
@@ -78,14 +113,20 @@ class WidgetTool extends React.Component {
     render() {
         console.log(this.props.widgets);
         console.log(Object.isFrozen(this.props));
+        //spread operator in ES2015, passes this.props as individual props to the WidgetTable component
         return <div>
     <h1>Widget Tool</h1>
-    <WidgetTable widgets={this.state.widgets} onSave={this.saveWidget} onEdit={this.editWidget} onCancel={this.cancelWidget} editRowId={this.state.editRowId} onDelete={this.deleteWidget} />
-    <WidgetForm onSubmit={this.saveWidget}/>
+    <WidgetTable {...this.props} /> 
+    <WidgetForm onSubmit={this.props.onSave}/>
 </div>;
     }
 
 }
-
+//React-Redux(connect and Provider component)
+//connect function: produces a container component that connects the application state to the action dispatch functions
+const WidgetToolContainer = connect(mapStateToProps, mapDispatchToProps)(WidgetTool);
+//Provider component: pass in the store and make it available to all container components in the tree via React's context feature
 ReactDOM.render(
-<WidgetTool />, document.querySelector('main'));
+<Provider store={appStore}>
+        <WidgetToolContainer />
+    </Provider>, document.querySelector('main'));
